65d95168e0405e033561975c191d8962e5ce87bb
Utente <Utente@192.168.1.53>
2021-03-08 11:22:48 +0100

    Seconda parte

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -14,6 +14,41 @@ public class Libretto {
 		this.voti.add(v);
 	}
 	
+	/* Questi due ci fanno schifo, non prendiamo cattive abitudini
+	public void stampaVotiUguali (int punteggio) {
+		// il libretto stampa da solo i voti
+		System.out.println();
+	}
+	
+	public String votiUguali(int punteggio) {
+		// calcla una stringa che contiene i voti
+		// sarà poi il chiamante a stampare
+		// -> Solo il NOME?
+		// -> Tutto il voto.toString()
+	}
+	*/
+	
+	public List<Voto> listaVotiUguali (int punteggio){
+		// restituisce solo i voti uguali al criterio
+		List<Voto> risultato = new ArrayList<>();
+		for (Voto v : this.voti) {
+			if (v.getVoto() == punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato;
+	}
+	
+	public Libretto votiUguali (int punteggio) {
+		Libretto risultato = new Libretto();
+		for (Voto v : this.voti) {
+			if (v.getVoto() == punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato;
+	}
+	
 	public String toString() {
 		String s = "";
 		for (Voto v : this.voti) {
